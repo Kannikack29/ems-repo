b1a815def68c867c1731185ea6b2b84e09198276
Kannika C K <kannikakumar2979@gmail.com>
2026-02-01 07:45:35 +0530

    Delete employee-management-system directory

--- employee-management-system/src/main/java/org/jsp/employee_management_system/exception/IdNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.jsp.employee_management_system.exception;
-
-public class IdNotFoundException extends RuntimeException{
-	public IdNotFoundException(String msg) {
-		super(msg);
-	}
-}
--- employee-management-system/src/main/java/org/jsp/employee_management_system/exception/NameValidationException.java
+++
@@ -1,9 +0,0 @@
-package org.jsp.employee_management_system.exception;
-
-public class NameValidationException extends RuntimeException{
-	
-	public NameValidationException(String msg) {
-		super(msg);
-	}
-
-}

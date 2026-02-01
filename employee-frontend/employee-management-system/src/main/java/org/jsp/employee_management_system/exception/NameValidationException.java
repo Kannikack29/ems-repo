4ad02b6e8cf3e58b37d7d2cd0618afd9034f9837
Kannika C K <kannikakumar2979@gmail.com>
2026-02-01 08:28:28 +0530

    next commit

---
+++ employee-frontend/employee-management-system/src/main/java/org/jsp/employee_management_system/exception/NameValidationException.java
@@ -0,0 +1,9 @@
+package org.jsp.employee_management_system.exception;
+
+public class NameValidationException extends RuntimeException{
+	
+	public NameValidationException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ employee-management-system/src/main/java/org/jsp/employee_management_system/exception/IdNotFoundException.java
@@ -0,0 +1,7 @@
+package org.jsp.employee_management_system.exception;
+
+public class IdNotFoundException extends RuntimeException{
+	public IdNotFoundException(String msg) {
+		super(msg);
+	}
+}
